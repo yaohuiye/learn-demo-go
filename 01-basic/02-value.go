ec3642a1db6b19c0cb4f04cb4d1b94cddfb3a4ff
tshe <yaohui@tshe.com>
2019-10-24 18:36:13 +0800

    init go project

---
+++ 01-basic/02-value.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("what's your name?" + "yaohuiye")
+	fmt.Println("7.0/3.0=", 7.0/3.0)
+	fmt.Println("7.0/3=", 7.0/3)
+	fmt.Println("7/3=", 7/3)
+	fmt.Println("7%3=", 7%3)
+
+	fmt.Println(!true)
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# learn-demo-go
-Go语言的学习demo
+#### 项目名称
+收集在学习go语言的过程中写的各种例子
+``coding right now``
+
+
+#### 学习链接
+- [gobyexample.com](https://gobyexample.com)
+#### 目录
+- 01-basic
